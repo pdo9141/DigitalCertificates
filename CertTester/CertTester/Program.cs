003e5e4dce8e67fec6d5d681a600c7faa0aac007
Mason Do <pdo9141@gmail.com>
2018-03-03 12:17:48 -0800

    Initial commit

---
+++ CertTester/CertTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CertTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
