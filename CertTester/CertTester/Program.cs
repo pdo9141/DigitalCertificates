3c6373505bd69f92a0dff808c48a06441d23c58c
Mason Do <pdo9141@gmail.com>
2018-03-03 18:49:55 -0800

    Adding validity check

--- CertTester/CertTester/Program.cs
+++ CertTester/CertTester/Program.cs
@@ -24,17 +24,21 @@ namespace CertTester
             // cert with private key
             var pfxPath = "files/modotech-test-cert.pfx";
             var certPassword = "123456789";
-            X509Certificate2 cert = new X509Certificate2(File.ReadAllBytes(pfxPath), certPassword);
+            var privateCert = new X509Certificate2(File.ReadAllBytes(pfxPath), certPassword);
 
-            SignXmlDocumentWithCertificate(doc, cert);
+            SignXmlDocumentWithCertificate(doc, privateCert);
+            
+            // XML document is now signed, validate it with the public key
+            var signedXmlPath = "files/AfterSigned.xml";
+            //signedXmlPath = "files/SignedButTampered.xml";
+            var signedXmlDoc = new XmlDocument();
+            signedXmlDoc.LoadXml(File.ReadAllText(signedXmlPath));
+            ValidateSignedXml(signedXmlDoc);
 
-            Console.WriteLine(doc.OuterXml);
-
-            File.WriteAllText(@"C:\temp\AfterSigned.xml", doc.OuterXml);
-
+            Console.WriteLine("Done");
             Console.Read();
         }
-
+        
         static void SignXmlDocumentWithCertificate(XmlDocument doc, X509Certificate2 cert)
         {
             var signedXml = new SignedXml(doc);
@@ -56,6 +60,27 @@ namespace CertTester
             // sign the document with computed signature
             var xmlSig = signedXml.GetXml();
             doc.DocumentElement.AppendChild(doc.ImportNode(xmlSig, true));
+        }
+
+        private static void ValidateSignedXml(XmlDocument doc)
+        {
+            // public key, has no password
+            var pemPath = "files/modotech-test-cert-public.pem";
+            var publicCert = new X509Certificate2(pemPath);
+
+            try
+            {
+                var signedXml = new SignedXml(doc);
+                var signatureNode = doc.GetElementsByTagName("Signature")[0];
+                signedXml.LoadXml((XmlElement)signatureNode);
+
+                var isValid = signedXml.CheckSignature(publicCert, true);
+                Console.WriteLine("Document validity is {0}", isValid);
+            }
+            catch
+            {
+                Console.WriteLine("Failed to validate");
+            }
         }
     }
 }
